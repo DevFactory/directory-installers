33f58abac98de9f00fb2d9f032ab932042d9a478
Chris Custine <ccustine@apache.org>
2007-12-20 22:24:30 +0000

    DIRSERVER-1114
    git-svn-id: https://svn.apache.org/repos/asf/directory/installers/trunk@606040 13f79535-47bb-0310-9956-ffa450edef68

--- apacheds-noarch/src/main/java/org/apache/directory/server/Service.java
+++ apacheds-noarch/src/main/java/org/apache/directory/server/Service.java
@@ -20,24 +20,15 @@
 package org.apache.directory.server;
 
 
-import java.io.File;
-import java.util.Properties;
-
-import javax.naming.Context;
-import javax.naming.directory.InitialDirContext;
-
 import org.apache.directory.daemon.DaemonApplication;
 import org.apache.directory.daemon.InstallationLayout;
-import org.apache.directory.server.configuration.MutableServerStartupConfiguration;
-import org.apache.directory.server.core.configuration.ShutdownConfiguration;
-import org.apache.directory.server.core.configuration.SyncConfiguration;
-import org.apache.directory.server.jndi.ServerContextFactory;
-
+import org.apache.directory.server.configuration.ApacheDS;
+import org.apache.xbean.spring.context.FileSystemXmlApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.io.File;
 
 
 /**
@@ -49,58 +40,29 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class Service implements DaemonApplication
 {
     private static final Logger log = LoggerFactory.getLogger( Service.class );
-    private Properties env;
     private Thread workerThread = null;
     private SynchWorker worker = new SynchWorker();
-    private MutableServerStartupConfiguration cfg;
-    private boolean startNoWait = false;
-
+    private ApacheDS apacheDS;
 
     public void init( InstallationLayout install, String[] args ) throws Exception
     {
         printBanner();
         long startTime = System.currentTimeMillis();
 
-//        if ( install != null )
-//        {
-//            log.info( "server: loading settings from ", install.getConfigurationFile() );
-//            ApplicationContext factory = null;
-//            factory = new FileSystemXmlApplicationContext( install.getConfigurationFile().toURL().toString() );
-//            cfg = ( MutableServerStartupConfiguration ) factory.getBean( "configuration" );
-//            env = ( Properties ) factory.getBean( "environment" );
-//        }
-//        else if ( args.length > 0 && new File( args[0] ).exists() ) // hack that takes server.xml file argument
-        if ( args.length > 0 && new File( args[0] ).exists() ) // hack that takes server.xml file argument
-        {
-            log.info( "server: loading settings from ", args[0] );
-            ApplicationContext factory = null;
-            factory = new FileSystemXmlApplicationContext( new File( args[0] ).toURI().toURL().toString() );
-            cfg = ( MutableServerStartupConfiguration ) factory.getBean( "configuration" );
-            env = ( Properties ) factory.getBean( "environment" );
-        }
-        else
-        {
-            log.info( "server: using default settings ..." );
-            env = new Properties();
-            cfg = new MutableServerStartupConfiguration();
-        }
+        log.info( "server: loading settings from ", args[0] );
+        ApplicationContext factory;
+        File configFile = new File( args[0] );
+        factory = new FileSystemXmlApplicationContext( configFile.toURI().toURL().toString() );
+        apacheDS = (ApacheDS) factory.getBean( "apacheDS" );
+        File workingDirFile = new File( configFile.getParentFile(), "work" );
+        apacheDS.getDirectoryService().setWorkingDirectory( workingDirFile );
+        apacheDS.startup();
 
-        env.setProperty( Context.PROVIDER_URL, "ou=system" );
-        env.setProperty( Context.INITIAL_CONTEXT_FACTORY, ServerContextFactory.class.getName() );
-
-        if ( install != null )
-        {
-            cfg.setWorkingDirectory( install.getPartitionsDirectory() );
-        }
-
-        env.putAll( cfg.toJndiEnvironment() );
-        new InitialDirContext( env );
-
-        if ( cfg.getSynchPeriodMillis() > 0 )
+        if ( apacheDS.getSynchPeriodMillis() > 0 )
         {
             workerThread = new Thread( worker, "SynchWorkerThread" );
         }
-        
+
         if ( log.isInfoEnabled() )
         {
             log.info( "server: started in {} milliseconds", ( System.currentTimeMillis() - startTime ) + "" );
@@ -110,8 +72,8 @@ public class Service implements DaemonApplication
 
     public void synch() throws Exception
     {
-        env.putAll( new SyncConfiguration().toJndiEnvironment() );
-        new InitialDirContext( env );
+//        env.putAll( new SyncConfiguration().toJndiEnvironment() );
+//        new InitialDirContext( env );
     }
 
 
@@ -127,23 +89,7 @@ public class Service implements DaemonApplication
 
     public void stop( String[] args ) throws Exception
     {
-        if ( workerThread != null )
-        {
-            worker.stop = true;
-            synchronized ( worker.lock )
-            {
-                worker.lock.notify();
-            }
-    
-            while ( startNoWait && workerThread.isAlive() )
-            {
-                log.info( "Waiting for SynchWorkerThread to die." );
-                workerThread.join( 500 );
-            }
-        }
-
-        env.putAll( new ShutdownConfiguration().toJndiEnvironment() );
-        new InitialDirContext( env );
+        apacheDS.getDirectoryService().shutdown();
     }
 
 
@@ -151,7 +97,7 @@ public class Service implements DaemonApplication
     {
     }
 
-    
+
     class SynchWorker implements Runnable
     {
         Object lock = new Object();
@@ -166,7 +112,7 @@ public class Service implements DaemonApplication
                 {
                     try
                     {
-                        lock.wait( cfg.getSynchPeriodMillis() );
+                        lock.wait( apacheDS.getSynchPeriodMillis() );
                     }
                     catch ( InterruptedException e )
                     {
