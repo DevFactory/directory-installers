b29d204d2e681c58d3276b8ea572e2ba757996a7
Emmanuel Lecharny <elecharny@apache.org>
2008-01-30 22:51:36 +0000

    o Some more Attributes removal. o Adding serialization for ServerEntry
    git-svn-id: https://svn.apache.org/repos/asf/directory/installers@616938 13f79535-47bb-0310-9956-ffa450edef68

--- branches/bigbang/apacheds-noarch/apacheds.sh
+++ branches/bigbang/apacheds-noarch/apacheds.sh
@@ -4,6 +4,9 @@ if [ -e target/apacheds-noarch-installer-1.5.2-SNAPSHOT-app.jar ] ; then
 else
   echo uber jar not found need to build it
   mvn clean assembly:assembly
+  jar -xvf target/apacheds-noarch-installer-1.5.2-SNAPSHOT-app.jar META-INF/MANIFEST.MF
+  echo "Class-path: myinterceptor.jar" >> META-INF/MANIFEST.MF 
+  jar -uf target/apacheds-noarch-installer-1.5.2-SNAPSHOT-app.jar  META-INF/MANIFEST.MF
 fi
 
 java -Dlog4j.debug -Dlog4j.configuration=file:./log4j.properties -jar target/apacheds-noarch-installer-1.5.2-SNAPSHOT-app.jar server.xml 
