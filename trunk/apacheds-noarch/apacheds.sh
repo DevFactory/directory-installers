5dd3d6d408cd43098d8c3a4b254b46ef1b806ef4
Alex Karasulu <akarasulu@apache.org>
2008-04-16 07:49:08 +0000

    remove trunk to replace with bigbang
    git-svn-id: https://svn.apache.org/repos/asf/directory/installers@648593 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/apacheds-noarch/apacheds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -e target/apacheds-noarch-installer-1.5.2-SNAPSHOT-app.jar ] ; then
-  echo uber jar exists
-else
-  echo uber jar not found need to build it
-  mvn clean install
-fi
-
-java -Dlog4j.debug -Dlog4j.configuration=file:./log4j.properties -jar target/apacheds-noarch-installer-1.5.2-SNAPSHOT-app.jar target/plan/server.xml 
