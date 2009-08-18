8ea9e830775bab3cbf495577d1fead5a2b7def48
Alex Karasulu <akarasulu@apache.org>
2009-08-18 08:03:51 +0000

    upgrade versions for apacheds
    git-svn-id: https://svn.apache.org/repos/asf/directory/installers/trunk@805310 13f79535-47bb-0310-9956-ffa450edef68

--- apacheds-noarch/apacheds.sh
+++ apacheds-noarch/apacheds.sh
@@ -17,11 +17,11 @@
 # specific language governing permissions and limitations
 # under the License.
 
-if [ -e target/apacheds-noarch-installer-1.5.5-SNAPSHOT-app.jar ] ; then
+if [ -e target/apacheds-noarch-installer-1.5.5-app.jar ] ; then
   echo uber jar exists
 else
   echo uber jar not found need to build it
   mvn clean install
 fi
 
-java -Dlog4j.debug -Dlog4j.configuration=file:./log4j.properties -jar target/apacheds-noarch-installer-1.5.5-SNAPSHOT-app.jar target/plan/server.xml 
+java -Dlog4j.debug -Dlog4j.configuration=file:./log4j.properties -jar target/apacheds-noarch-installer-1.5.5-app.jar target/plan/server.xml 
